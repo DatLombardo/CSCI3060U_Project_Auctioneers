0694a638ab70723b90b9e8c5860d5284841c9fa8
DatLombardo <Michael.Lombardo@uoit.net>
2018-03-10 15:55:32 -0500

    Addede Phase 4 skeleton, included testing txt files.

--- src/Parser.java
+++ src/Parser.java
@@ -1,13 +1,32 @@
 import java.io.*;
 import java.util.*;
 
+/*
+  Auctioneers
+  Michael Lombardo, Joseph Robertson, Michael Setnyk
+  Parser.java
+ */
 public class Parser{
+    //Container definition.
   public Map<String, User> userList = new HashMap<String, User>();
   public Map<String, Item> itemList = new HashMap<String, Item>();
+  public ArrayList<String> transactionFile = new ArrayList<String>();
+
+    /**
+     * Default General Constructor for Parser class, defined to allow for system to use Parser anywhere.
+     */
   public Parser(){ }
+
+    /**
+     * readUserFile
+     * Reads in user file and stores all user instances into userList map.
+     * @param path
+     * @return userList map holding all instances of Users with key (username [unfilled])
+     * @throws IOException
+     */
   public Map<String, User> readUserFile(String path) throws IOException{
         BufferedReader in = new BufferedReader(new FileReader(path));
-        String line = "";
+        String line;
         while ((line = in.readLine()) != null) {
             User newUser = new User(line);
             this.userList.put(removeSpaceFill(newUser.getUsername()), newUser);
@@ -16,18 +35,109 @@ public class Parser{
         return this.userList;
 	}
 
+    /**
+     * readItemFile
+     * Reads in item file and stores all item instances into itemList map.
+     * @param path
+     * @return itemList map holding all instances of Items with key (itemName+sellerName [unfilled])
+     * @throws IOException
+     */
 	public Map<String, Item> readItemFile(String path) throws IOException{
         BufferedReader in = new BufferedReader(new FileReader(path));
-        String line = "";
+        String line;
         while ((line = in.readLine()) != null) {
             Item newItem = new Item(line);
             String newKey = removeSpaceFill(newItem.getItemName()) + removeSpaceFill(newItem.getSellerName());
             this.itemList.put(newKey, newItem);
-    }
+        }
         in.close();
         return this.itemList;
 	}
 
+    /**
+     * readTransactionFile
+     * Reads in daily transaction file and stores into transactionFile arrayList.
+     * @param path
+     * @return arrayList of dailyTransaction file requests
+     * @throws IOException
+     */
+	public ArrayList<String> readTransactionFile(String path) throws IOException{
+        BufferedReader in = new BufferedReader(new FileReader(path));
+        String line = "";
+        while ((line = in.readLine()) != null) {
+            this.transactionFile.add(line);
+        }
+        in.close();
+        return this.transactionFile;
+    }
+
+    /**
+     *  clearFile
+     *  Called to remove contents from any file passed.
+     * @param path - desired path to file of removal
+     * @throws IOException
+     */
+    public void clearFile(String path) throws IOException{
+        PrintWriter writer = new PrintWriter(path);
+        writer.print("");
+        writer.close();
+    }
+
+    /**
+     * writeAccounts
+     * Called to write Current User Accounts file after all user accounts manipulation is complete.
+     * Assumes that the file is empty, need to call clearFile
+     * Format: UUUUUUUUUUUUUUU_TT_CCCCCCCCC
+     * @param path - desired path for write
+     * @throws IOException
+     */
+    public void writeAccounts(String path) throws IOException{
+        File writer = new File(path);
+        FileOutputStream fileOut = new FileOutputStream(writer);
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fileOut));
+
+        User currUser;
+        for (Map.Entry<String, User> entry : userList.entrySet()) {
+            currUser = entry.getValue();
+            bw.write(currUser.userFileString());
+            bw.newLine();
+        }
+        bw.close();
+
+    }
+
+    /**
+     * writeItems
+     * Called to write Availiable Items File
+     * Assumes that the file is empty, need to call clearFile.
+     * Format: IIIIIIIIIIIIIIIIIII_SSSSSSSSSSSSSSS_UUUUUUUUUUUUUU_DDD_PPPPPP
+     * @param path - desired path for write
+     * @throws IOException
+     */
+    public void writeItems(String path) throws IOException{
+        File writer = new File(path);
+        FileOutputStream fileOut = new FileOutputStream(writer);
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fileOut));
+
+        Item currItem;
+        for (Map.Entry<String, Item> entry : this.itemList.entrySet()) {
+            currItem = entry.getValue();
+            bw.write(currItem.itemFileString());
+            bw.newLine();
+        }
+        bw.close();
+
+    }
+
+    /**
+     * removeSpaceFill
+     * Removes extra spacing from the end of usernames / itemnames to allow for key generation for the
+     * itemList and userList.
+     * @param input - arguement to be trimmed.
+     * @return trimmed input.
+     */
     public String removeSpaceFill(String input){
         if( input == null)
             return null;
@@ -37,5 +147,38 @@ public class Parser{
                 break;
         }
         return input.substring( 0, len);
+    }
+
+    /**
+     * fillFunds
+     * Called to add additional zeroes to the left-side of the funds value for file writing.
+     * @param input
+     * @return
+     */
+    public String fillFunds(double input){
+        String filledInput = String.format("%09.2f", input);
+        return filledInput;
+    }
+
+    /**
+     * fillBids
+     * Called to add additional zeroes to the left-side of the bid value for file writing.
+     * @param input
+     * @return
+     */
+    public String fillBids(double input){
+        String filledInput = String.format("%07.2f", input);
+        return filledInput;
+    }
+
+    /**
+     * fillDays
+     * Called to add additional zeroes to the left-side of the daysToExpiry value for file writing.
+     * @param input
+     * @return
+     */
+    public String fillDays(int input){
+        String filledInput = String.format("%03d", input);
+        return filledInput;
     }
 }
--- src/TransactionProcessor.java
+++ src/TransactionProcessor.java
@@ -2,79 +2,139 @@ import java.io.*;
 import java.util.*;
 //completly untested
 //not sure about the numbers
+
+/*
+  Auctioneers
+  Michael Lombardo, Joseph Robertson, Michael Setnyk
+  TransactionProcessor.java
+ */
 public class TransactionProcessor{
+    /**
+     * TransactionProcessor
+     * General Constructor for each transaction request, read in the line and complete request.
+     * @param line
+     * @param items
+     * @param users
+     */
     public void TransactionProcessor(String line, Map<String, Item> items, Map<String, User> users){
         int action=Integer.parseInt(line.substring(0,2));
         Parser parser = new Parser();
+        String username, type, buyer, seller, itemname;
+        double bid;
         switch(action){
             case 0:
                 //logout
                 break;
-            case 10;
+            case 10:
                 //login
                 break;
             case 1:
                 //create
-                String username=line.substring(3,18);
-                String type=line.substring(19,21);
+                username=line.substring(3,18);
+                type=line.substring(19,21);
                 double credit=Double.parseDouble(line.substring(22,31));
                 create(username,type,credit);
                 break;
             case 2:
                 //delete
-                String username=line.substring(3,18);
+                username=line.substring(3,18);
                 delete(username);
                 break;
             case 3:
                 //advertise
-                String seller=line.substring(3,22);
-                String itemname=line.substring(23,36);
-                double bid=Double.parseDouble(line.substring(41,47));
+                seller = line.substring(3,22);
+                itemname=line.substring(23,36);
+                bid = Double.parseDouble(line.substring(41,47));
                 int days=Integer.parseInt(line.substring(37,40));
                 advertise(seller,itemname,bid,days);
                 break;
             case 4:
                 //bid
-                String seller=line.substring(23,37);
-                String buyer=line.substring(39,53);
-                String itemname=line.substring(3,22);
-                double bid=Double.parseDouble(line.substring(54,60));
+                seller = line.substring(23,37);
+                buyer = line.substring(39,53);
+                itemname = line.substring(3,22);
+                bid = Double.parseDouble(line.substring(54,60));
                 bid(seller,buyer,itemname,bid);
                 break;
             case 5:
                 //refund
-                String seller=line.substring(19,34);
-                String buyer=line.substring(3,18);
-                double amountDouble.parseDouble(line.substring(35,44));
+                seller = line.substring(19,34);
+                buyer = line.substring(3,18);
+                double amount = Double.parseDouble(line.substring(35,44));
                 refund(seller,buyer,amount);
                 break;
             case 6:
                 //addCredit
-                String username=line.substring(3,18);
-                double credit=Double.parseDouble(line.substring(22,31));
-                addCreadit(username,balance);
+                username = line.substring(3,18);
+                credit = Double.parseDouble(line.substring(22,31));
+                addCredit(username, credit);
                 break;
         }
     }
+
+    /**
+     * processTransaction
+     */
     public void processTransation(){
 
     }
+
+    /**
+     * create
+     * @param username
+     * @param type
+     * @param credit
+     */
     private void create(String username, String type, double credit){
 
     }
+
+    /**
+     * delete
+     * @param username
+     */
     private void delete(String username){
 
     }
+
+    /**
+     * advertise
+     * @param seller
+     * @param itemname
+     * @param bid
+     * @param days
+     */
     private void advertise(String seller, String itemname, double bid, int days){
 
     }
+
+    /**
+     * bid
+     * @param seller
+     * @param buyer
+     * @param itemname
+     * @param bid
+     */
     private void bid(String seller, String buyer, String itemname, double bid){
 
     }
+
+    /**
+     * refund
+     * @param seller
+     * @param buyer
+     * @param amount
+     */
     private void refund(String seller, String buyer, double amount){
 
     }
-    private void addCreadit(String username, double balance){
+
+    /**
+     * addCredit
+     * @param username
+     * @param balance
+     */
+    private void addCredit(String username, double balance){
 
     }
 }
