1e2ab91256f56ef0232d89a632e61ca716e74844
michaelSetnyk <msetnyk@gmail.com>
2018-03-13 22:10:53 -0400

    Docummented main and reads file paths as command line arguments

--- src/Backend.java
+++ src/Backend.java
@@ -5,18 +5,36 @@ import java.util.*;
   Auctioneers
   Michael Lombardo, Joseph Robertson, Michael Setnyk
   Backend.java
+
+  This program is the backend for an auction house system.
+  A merged transaction file, user file and items file (in that order)
+  are all sent in as arguments to the auction house system
+  The system applies changes made to the user and items files
+  Changes are specifed in the transaction file.  All changes are
+  recorded in the system when a login transaction is read in.
+  Changes are official when a logout transaction is read.
+  If a user is deleted before they make any transactions then those
+  transactions will never exist.
  */
 public class Backend{
 	//Containers for user and item class
 	public static void main(String[] args){
 		try{
-			Parser parser = new Parser();
-			Map<String, User> userList = parser.readUserFile("src/userlist.txt");
-			Map<String, Item> itemList = parser.readItemFile("src/itemlist.txt");
-			ArrayList<String> transactionFile = parser.readTransactionFile("src/transactionFile.txt");
-			parser.clearFile("src/transactionFile.txt");
+			if(args.length!=3){
+				System.out.println("Please enter a transaction file,  user file, then a items file");
+				return;
+			}
+			String transactionPath = args[0];
+			String usersPath = args[1];
+			String itemsPath = args[2];
 
-			/* How to Access the user and item lists.
+
+			Parser parser = new Parser();
+			Map<String, User> userList = parser.readUserFile(usersPath);
+			Map<String, Item> itemList = parser.readItemFile(itemsPath);
+			ArrayList<String> transactionFile = parser.readTransactionFile(transactionPath);
+			parser.clearFile(transactionPath);
+
 			User currUser;
 			System.out.println(userList);
 			for (Map.Entry<String, User> entry : userList.entrySet()) {
@@ -30,7 +48,6 @@ public class Backend{
 				System.out.println(currItem.getItemName() + currItem.getSellerName()  + currItem.getHighestBidder()
 						+ parser.fillDays(currItem.getDaysToExpiry()) + " " +  parser.fillBids(currItem.getBid()));
 			}
-			*/
 		}catch(IOException e){
 		  e.printStackTrace();
 		}
