04328ead1d0c036afc55efffe4faadbe3684bd58
michaelSetnyk <msetnyk@gmail.com>
2018-03-26 18:52:44 -0400

    fixed up parser test and backend tests

--- src/main/ItemTest.java
+++ src/main/ItemTest.java
@@ -1,24 +1,43 @@
-import java.io.*;
 import org.junit.Test;
-import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
 
 public class ItemTest {
 
-  @Test
-  public void test1(){
-    //test to see if no arguments do not work
-    // Backend.main(args);
-    assertEquals(1,2);
-  }
-
-
-
-  /*  @Test
-    public void test2()throws IOException{
-      //test to see if no arguments do not work
-       String[] args = new String[] {"transactionFile.txt","userlist.txt","itemlist.txt"};
-
-       Backend.main(args);
-    }*/
+    @Test
+    public void test1() {
+	Item A = new Item("kite surfing set          seller01        username01      014 0300.00");
+	assertEquals("kite surfing set         ", A.getItemName());
+    }
+    @Test
+    public void test2() {
+	Item A = new Item("kite surfing set          seller01        username01      014 0300.00");
+	assertEquals("seller01       ", A.getSellerName());
+    }
+    @Test
+    public void test3() {
+	Item A = new Item("kite surfing set          seller01        username01      014 0300.00");
+	assertEquals("username01     ", A.getHighestBidder());
+    }
+    @Test
+    public void test4() {
+	Item A = new Item("kite surfing set          seller01        username01      014 0300.00");
+	assertEquals(14, A.getDaysToExpiry());
+    }
+    @Test
+    public void test5() {
+	Item A = new Item("kite surfing set          seller01        username01      014 0300.00");
+	assertTrue(300.0==A.getBid());
+    }
+    @Test
+    public void test6() {
+	Item A = new Item("kite surfing set          seller01        username01      014 0300.00");
+	A.setHighestBidder("newBidder     ", 400.0);
+	assertTrue((400.0 == A.getBid()) && (A.getHighestBidder().equals("newBidder     ")));
+    }
+    @Test
+    public void test7() {
+	Item A = new Item("kite surfing set          seller01        username01      014 0300.00");
+	assertEquals("kite surfing set          seller01        username01      014 0300.00", A.itemFileString());
+    }
 }
