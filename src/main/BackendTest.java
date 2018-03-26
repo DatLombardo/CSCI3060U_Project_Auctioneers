b8bccbb43390b0f92a10ef6fa1de1b0c9ee3305f
michaelSetnyk <msetnyk@gmail.com>
2018-03-26 18:23:24 -0400

    more main tests

--- src/main/BackendTest.java
+++ src/main/BackendTest.java
@@ -2,12 +2,13 @@ import java.io.*;
 import org.junit.Test;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class BackendTest {
 
   @Test(expected = NullPointerException.class)
   public void test1()throws NullPointerException{
-    //test to see if no arguments do not work
+    //test to see if null arguments are rejected
      String[] args=null;
       Backend b = new Backend(args);
 
@@ -15,18 +16,19 @@ public class BackendTest {
 
   @Test
   public void test2()throws NullPointerException{
-    //test to see if no arguments do not work
+    //test to see if 3 arguments work
      String[] args=new String[]{"transactionFile.txt","userlist.txt","itemlist.txt"};
       Backend b = new Backend(args);
-      return;
+      assertEquals(args.length,3);
   }
 
 
-  /*  @Test
-    public void test2()throws IOException{
-      //test to see if no arguments do not work
-       String[] args = new String[] {"transactionFile.txt","userlist.txt","itemlist.txt"};
+    @Test
+    public void test3(){
+      //test to see if not three arguments work
+       String[] args = new String[] {"transactionFile.txt"};
+       Backend b = new Backend(args);
+       assertNotEquals(args.length,3);
 
-       Backend.main(args);
-    }*/
+    }
 }
